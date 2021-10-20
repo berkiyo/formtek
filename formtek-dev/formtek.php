7fd540342087022ae1d4821ec6dfbd2e25e74a9e
berkiyo <berk@tekbyte.net>
2021-10-21 09:21:16 +1100

    Small changes to the info block

--- formtek-dev/formtek.php
+++ formtek-dev/formtek.php
@@ -6,6 +6,21 @@ Description: The definitive contact form plugin
 Version: 0.1
 Author: Berk Dogan
 Author URI: http://tekbyte.net
+
+Copyright 2021 Berk Dogan (email : berk@tekbyte.net)
+Formtek is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Formtek is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Formtek. If not, see (https://www.gnu.org/licenses/).
+
 */
 
 
