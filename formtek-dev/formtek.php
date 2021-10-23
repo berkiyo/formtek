d7fa4e663fa817d27aae520392bbe4401602772b
berkiyo <berk@tekbyte.net>
2021-10-23 23:38:13 +1100

    Test changes for Textastic

--- formtek-dev/formtek.php
+++ formtek-dev/formtek.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formtek
 Plugin URI: N/A
-Description: The definitive contact form plugin
+Description: An easy to use contact form plugin that is extensible, secure and stays out of your way.
 Version: 0.1
 Author: Berk Dogan
 Author URI: http://tekbyte.net
@@ -23,66 +23,40 @@ along with Formtek. If not, see (https://www.gnu.org/licenses/).
 
 */
 
-
-/**
- * HTML Form Code (currently WIP)
- */
-/* 
-function html_form_code() {
-    echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
-    echo '<p>';
-    echo 'Your Name (required) <br />';
-    echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    echo '<p>';
-    echo 'Your Email (required) <br />';
-    echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    echo '<p>';
-    echo 'Subject Line (required) <br />';
-    echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
-    echo '</p>';
-    echo '<p>';
-    echo 'Your Message (required) <br />';
-    echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
-    echo '</p>';
-    echo '<p><input type="submit" name="cf-submitted" value="Send"/></p>';
-    echo '</form>';
+// absolute path that is definied by WordPress
+if (! function_exists('add_action')) {
+    echo 'Forbidden - You cannot access this file';
+    die;
 }
-*/
 
-/**
- * The PHP mailer
- */
-function mailer() {
 
-    // if the submit button is clicked, send the email
-    if ( isset( $_POST['cf-submitted'] ) ) {
+// let's get into the OOP side of things
+class FormtekPlugin {
 
-        // sanitize form values
-        $name    = sanitize_text_field( $_POST["cf-name"] );
-        $email   = sanitize_email( $_POST["cf-email"] );
-        $subject = sanitize_text_field( $_POST["cf-subject"] );
-        $message = esc_textarea( $_POST["cf-message"] );
-
-        // get the blog administrator's email address
-        $to = get_option( 'admin_email' );
-
-        $headers = "From: $name <$email>" . "\r\n";
-
-        // If email has been process for sending, display a success message
-        if ( wp_mail( $to, $subject, $message, $headers ) ) {
-            echo '<div>';
-            echo '<p>Thanks for contacting me, expect a response soon.</p>';
-            echo '</div>';
-        } else {
-            echo 'An unexpected error occurred';
-        }
+    function activate() {
+        echo 'The plugin is now activated';
     }
+
+    function deactivate() {
+        echo 'The plugin is now deactivate';
+    }
+
+    function uninstall() {
+
+    }
+
 }
 
-/**
- * Need the short code here + do testing
- */
+if (class_exists('FormtekPlugin')) {
+    $formtekPlugin = new FormtekPlugin();
+}
+
+
+// activation
+register_activation_hook(__FILE__, array($formtekPlugin, 'activate')); // we pass an array, use our object and call the activate function (__FILE__ is global)
+
+// deactivation
+
+// uninstall
 
 ?>
